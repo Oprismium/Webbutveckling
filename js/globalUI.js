a04654cefee016ee150c2db4b3d6de2f1afab1aa
Oprismium <firepythhon@gmail.com>
2026-01-23 09:36:00 +0100

    Revert "Merge branch 'main' of"
    This reverts commit e2e7b6ceb5d0f3b8587cf5f13ca5572cf1dd76ed, reversing
    changes made to 14343ac1f14871ad71b5ca0ccfebc25819b9dcee.

--- js/globalUI.js
+++ js/globalUI.js
@@ -13,10 +13,7 @@ function initSidebar() {
     });
 }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 0dc8466bee2c78718d1438c08efbe9439b14d66a
 /* ================= THEME SYSTEM ================= */
 
 const themes = {

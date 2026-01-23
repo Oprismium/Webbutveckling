824ab1bc9f246e6e4de4d9a4e536f77021f3ed4b
Oprismium <firepythhon@gmail.com>
2026-01-23 09:35:49 +0100

    Revert "Commit"
    This reverts commit 191ee1243e39b9221d082cd564b1df3409d77b4f.

--- js/sidebarLoader.js
+++ js/sidebarLoader.js
@@ -7,5 +7,8 @@ async function loadSidebar() {
     initThemeControls();
 }
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 0dc8466bee2c78718d1438c08efbe9439b14d66a
 document.addEventListener('DOMContentLoaded', loadSidebar);

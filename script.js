191ee1243e39b9221d082cd564b1df3409d77b4f
Oprismium <firepythhon@gmail.com>
2026-01-23 08:43:01 +0100

    Commit

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // Sidebar hover behavior ONLY
-<<<<<<< HEAD
 
-=======
->>>>>>> 0dc8466bee2c78718d1438c08efbe9439b14d66a
 document.addEventListener("mousemove", event => {
     const sidebar = document.getElementById("sidebar");
     if (!sidebar) return;
